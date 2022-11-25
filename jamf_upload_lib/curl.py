768609318e3f32f088355b732597f35dd0321c5f
poojithait4 <118990294+poojithait4@users.noreply.github.com>
2022-11-25 08:57:44 -0800

    Update curl.py

--- jamf_upload_lib/curl.py
+++ jamf_upload_lib/curl.py
@@ -121,9 +121,9 @@ def request(method, url, auth, verbosity, data="", additional_headers="", xml=Fa
     subprocess.check_output(curl_cmd)
 
     r = namedtuple(
-        "r", ["headers", "status_code", "output"], defaults = [None, None, None]
+        "r", ["headers", "status_code", "output"]
     )
-    print(r())
+    r = r(None, None, None)
     print(r.headers)
     print(r.status_code)
     print(r.output)
